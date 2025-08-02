e2f3a83848e279faf7f0536774451fa44ac4e536
Vince-Munene <vincegachuru@gmail.com>
2025-08-02 21:01:44 +0300

    initial folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-5xl font-bold text-black">Hello, React + Tailwind!</h1>
+    </div>
+  )
+}
+
+export default App
