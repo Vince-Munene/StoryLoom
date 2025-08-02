5893280111ebbf28184de264cb582785e35c8f4f
Vince-Munene <vincegachuru@gmail.com>
2025-08-02 22:32:35 +0300

    added SignIn page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
+import SignIn from './components/SignIn';
+
 function App() {
   return (
-    <div className="p-8 text-center">
-      <h1 className="text-5xl font-bold text-black">Hello, React + Tailwind!</h1>
-    </div>
-  )
+    <SignIn />
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        midbrown: "#B1521E",
+        darkbrown: "#7F3608",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+    },
   },
   plugins: [],
 }
-
+}
